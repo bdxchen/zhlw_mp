71c783c25e74705092c976273d20ab1abf1baed9
weichen.wang <weichen.wang@zhaopin.com.cn>
2018-10-29 19:13:58 +0800

    小程序初始化

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import Vue from 'vue'
+import App from './App'
+import 'mpvue-weui/src/style/weui.css';
+
+Vue.config.productionTip = false
+App.mpType = 'app'
+
+const app = new Vue(App)
+app.$mount()
