49c494eceb14c5741249cd5a018faa8c828604bf
weichen.wang <weichen.wang@zhaopin.com.cn>
2018-10-31 15:54:43 +0800

    ESLint代码规范

--- src/store/index.js
+++ src/store/index.js
@@ -1,21 +1,18 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 
-import test from './module/sampleCode'
+import test from "./module/sampleCode";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-    
+  "state": {
   },
-  mutations: {
-    
+  "mutations": {
   },
-  actions: {
-    
+  "actions": {
   },
-  modules: {
+  "modules": {
     test
   }
-})
+});
