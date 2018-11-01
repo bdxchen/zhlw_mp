7a031a05d1b9869bfd85ba0b711b58b57d1c4864
weichen.wang <weichen.wang@zhaopin.com.cn>
2018-11-01 19:25:54 +0800

    无法拯救了，以后就直接用app.json好了。。

--- src/main.js
+++ src/main.js
@@ -13,32 +13,3 @@ Vue.use(MpvueRouterPatch);
 const app = new Vue(App);
 
 app.$mount();
-
-export default {
-  // 这个字段走 app.json
-  config: {
-    // 页面前带有 ^ 符号的，会被编译成首页，其他页面可以选填，我们会自动把 webpack entry 里面的入口页面加进去
-    pages: ["^pages/counter/main", "pages/me/main", "pages/hello/main"],
-    window: {
-      backgroundTextStyle: "light",
-      navigationBarBackgroundColor: "#ea5149",
-      navigationBarTitleText: "WeChat",
-      navigationBarTextStyle: "black"
-    },
-    tabBar: {
-      selectedColor: "#000",
-      list: [{
-        pagePath: "pages/index/main",
-        text: "主页",
-        iconPath: "static/img/pkq.png",
-        selectedIconPath: "static/img/pkq-active.png"
-      },
-      {
-        pagePath: "pages/me/main",
-        text: "个人",
-        iconPath: "static/img/jlq.png",
-        selectedIconPath: "static/img/jlq-active.png"
-      }]
-    }
-  }
-};
