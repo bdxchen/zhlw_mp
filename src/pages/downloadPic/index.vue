4045d0c3190a4968311c99948040cc814d9ac196
zhanghw <18831215676@163.com>
2018-12-10 18:26:31 +0800

    zhanghw

--- src/pages/downloadPic/index.vue
+++ src/pages/downloadPic/index.vue
@@ -1,6 +1,19 @@
 <template>
   <div>
     下载图片页面
+    
+    <div class="bottom">
+      <img class="img" src="/static/img/bottom.png">
+    </div>
+    <div class="bottom">
+      <img class="img" src="/static/img/bottom1.png">
+    </div>
+    <div class="bottom">
+      <img class="img" src="/static/img/topbg.png">
+    </div>
+    <div class="bottom">
+      <img class="img" src="/static/img/bottombg.png">
+    </div>
   </div>
 </template>
 
@@ -23,6 +36,15 @@ export default {
 };
 </script>
 
-<style>
-
+<style lang="scss">
+  
+  .bottom {
+    width: 100%;
+    height: 80px;
+    background: #e58d;
+    .img {
+      width: 100%;
+      height: 50px;
+    }
+  }
 </style>
