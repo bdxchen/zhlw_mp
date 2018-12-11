c8dd206ca6006d4335d20e48f7a72ab44690bc97
zhanghw <18831215676@163.com>
2018-12-11 22:38:41 +0800

    zhanghw

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -16,6 +16,7 @@
       <input type="text" class="form-control" v-model="motto" placeholder="v-model" />
       <input type="text" class="form-control" v-model.lazy="motto" placeholder="v-model.lazy" />
     </form>
+    <a href="/pages/index1/main" class="counter">index</a>
     <a href="/pages/counter/main" class="counter">去往Vuex示例页面</a>
     <a href="/pages/activity/main" class="counter">去往活动页面</a>
     <a href="/pages/appointment/main" class="counter">去往预约页面</a>
---
+++ src/pages/index1/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>{{motto}}</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      motto: "Hello World"
+    };
+  },
+
+  methods: {},
+
+  created() {}
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
