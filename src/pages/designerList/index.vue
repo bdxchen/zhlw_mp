5a948fd2ac1a4371172948441961bd42d546a4e0
zhanghw <18831215676@163.com>
2018-12-13 18:24:35 +0800

    设计师列表

--- src/pages/designerList/index.vue
+++ src/pages/designerList/index.vue
@@ -1,6 +1,15 @@
 <template>
-  <div>
-    设计师列表页面
+  <div class="content">
+    <div class="top">
+      <img class="topbg" src="/static/img/topbg.png" />
+    </div>
+    <div class="main">
+
+    </div>
+    <div class="bottom">
+      <img src="/static/img/bottombg.png" />
+    </div>
+    
   </div>
 </template>
 
@@ -23,6 +32,51 @@ export default {
 };
 </script>
 
-<style>
-
+<style lang="scss">
+page {
+  height: 100%;
+}
+.content{
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  .top{
+    width: 100%;
+    height: 350rpx;
+    position: relative;
+    display: flex;
+    align-items: center;
+    
+    .topbg {
+      width: 100%;
+      height: 100%;
+      position: absolute;
+      top: 0;
+      left: 0;
+      z-index: -1;
+    }
+  }
+  
+  .main{
+    width: 100%;
+    flex: 1;
+    overflow: auto;
+    
+    
+    
+  }
+  .bottom {
+    width: 100%;
+    height: 50px;
+    position: relative;
+    img {
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+    }
+  }
+}
 </style>
