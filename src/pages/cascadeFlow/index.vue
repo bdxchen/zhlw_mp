25aa850cc2c5e7fb5697ee79471d0d76ecc9bee5
bdxchen <bdxchen@gmail.com>
2019-01-03 01:42:09 +0800

    瀑布流

--- src/pages/cascadeFlow/index.vue
+++ src/pages/cascadeFlow/index.vue
@@ -3,9 +3,9 @@
     <div class="top">
       <image class="topbg" src="/static/img/topbg.png" mode="widthFix"/>
     </div>
-    <view class="swiper-tab">
-        <view class="swiper-tab-item" :class="{'active': currentTab==0}" data-current="0" @tap="clickTab">我的</view>
-        <view class="swiper-tab-item" :class="{'active': currentTab==1}" data-current="1" @tap="clickTab">我喜欢的</view>
+    <div class="title">我的作品</div>
+    <view style="display:none">
+      <image v-for="item in images" :key="item.id" :id="item.id" :src="item.image" @load="onImageLoad"></image>
     </view>
     <swiper 
       :current="currentTab" 
@@ -13,9 +13,6 @@
       @change="swiperTab"
       :style="{height:scroll_height}">
       <swiper-item >
-        <view style="display:none">
-          <image v-for="item in images" :key="item.id" :id="item.id" :src="item.image" @load="onImageLoad"></image>
-        </view>
         <scroll-view 
           class="scrollview" 
           scroll-y="true" 
@@ -40,30 +37,6 @@
                   <div class="like"><i class="icon iconfont">&#xe6a5;</i></div>
                   <div class="more">...</div>
                 </div>
-              </div>
-            </view>
-          </view>
-        </scroll-view>
-      </swiper-item>
-      <swiper-item>
-        <scroll-view 
-          class="scrollview" 
-          scroll-y="true" 
-          :style="{height:scrollH + 'px'}" 
-          @scrolltolower="loadImages">
-          <view style="width:100%">
-            <view class="img_item">
-              <div style="height: 90rpx"></div>
-              <div class="flowPic" v-for="item in col1" :key="item.id">
-                <image :src="item.image" :style="{height:item.height + 'rpx'}" />
-                <div class="imgbottom"></div>
-              </div>
-            </view>
-            <view class="img_item">
-              <div style="height: 90rpx"></div>
-              <div class="flowPic" v-for="item in col2" :key="item.id">
-                <image :src="item.image" :style="{height:item.height + 'rpx'}" ></image>
-                <div class="imgbottom"></div>
               </div>
             </view>
           </view>
@@ -106,12 +79,13 @@ export default {
     this.scrollH = 0
     let windowHeight = wx.getSystemInfoSync().screenHeight // 屏幕的高度
     let windowWidth = wx.getSystemInfoSync().screenWidth // 屏幕的宽度
-    this.scroll_height = windowHeight * 750 / windowWidth - 200 - 40 + 'rpx'
+    this.scroll_height = windowHeight * 750 / windowWidth - 200 - 100 + 'rpx'
+    console.log(windowWidth, windowHeight)
     wx.getSystemInfo({
         success: (res) => {
             let ww = res.windowWidth;
             let wh = res.windowHeight;
-            let scrollH = wh;
+            let scrollH = wh - 80;
             this.scrollH = scrollH
         }
     });
@@ -121,90 +95,62 @@ export default {
         this.getUserImg(res.data.user_id)
       }
     })
-    this.getMyLikes()
-  },
-  created() {
-    
   },
   methods: {
     getUserImg(user_id) {
       let params = {
         url: `/get_student_image/${user_id}/`,
       }
-      get(params).then(res=>{ 
+      get(params).then(res=>{
         console.log('getUserImg',res)
         this.myImgs = res
         this.loadImages()
       })
     },
-    getMyLikes() {
-      let params = {
-        url: '/get_user_like/',
-      }
-      get(params).then(res=>{ 
-        console.log('getMyLikes',res)
-        this.myLikes = res
-        this.loadImages()
-      })
-    },
-    swiperTab(e) {
-      console.log(e)
-      this.currentTab = e.target.current;
-      if(this.currentTab == 0) {
-        
-      }
-    },
-    //点击切换
-    clickTab(e) {
-      if (this.currentTab === e.currentTarget.dataset.current) {
-        return false;
-      } else {
-        this.currentTab = e.currentTarget.dataset.current
-      }
-    },
     onImageLoad(e) {
-        console.log(e.target.height)
-        let imageId = e.target.id;
-        let oImgW = e.target.width;         //图片原始宽度
-        let oImgH = e.target.height;        //图片原始高度
-        let imgWidth = this.imgWidth;       //图片设置的宽度
-        let scale = imgWidth / oImgW;       //比例计算
-        let imgHeight = oImgH * scale;      //自适应高度
-        let images = this.images;
-        let imageObj = null;
+      console.log(e.target.width)
+      let imageId = e.target.id;
+      let oImgW = e.target.width;         //图片原始宽度
+      let oImgH = e.target.height;        //图片原始高度
+      let imgWidth = this.imgWidth;       //图片设置的宽度
+      let scale = imgWidth / oImgW;       //比例计算
+      let imgHeight = oImgH * scale;      //自适应高度
+      let images = this.images;
+      let imageObj = null;
 
-        for (let i = 0; i < images.length; i++) {
-            let img = images[i];
-            if (img.id === imageId) {
-                imageObj = img;
-                break;
-            }
-        }
-
-        imageObj.height = imgHeight;
-        let loadingCount = this.loadingCount - 1;
-        let col1 = this.col1;
-        let col2 = this.col2;
-        if (this.col1H <= this.col2H) {
-            this.col1H += imgHeight;
-            col1.push(imageObj);
-        } else {
-            this.col2H += imgHeight;
-            col2.push(imageObj);
-        }
-        if (!loadingCount) {
-            this.images = [];
-        }
-        this.loadingCount = loadingCount;
-        this.col1 = col1;
-        this.col2 = col2;
+      for (let i = 0; i < images.length; i++) {
+          let img = images[i];
+          if (img.id === imageId) {
+              imageObj = img;
+              break;
+          }
+      }
+      imageObj.height = imgHeight;
+      let loadingCount = this.loadingCount - 1;
+      let col1 = this.col1;
+      let col2 = this.col2;
+      if (this.col1H <= this.col2H) {
+          this.col1H += imgHeight;
+          col1.push(imageObj);
+      } else {
+          this.col2H += imgHeight;
+          col2.push(imageObj);
+      }
+      if (!loadingCount) {
+          this.images = [];
+      }
+      this.loadingCount = loadingCount;
+      this.col1 = col1;
+      this.col2 = col2;
+      console.log(this.col1, this.col2)
     },
 
     loadImages() {
         let images = this.myImgs
         let baseId = "img-" + (+new Date());
         for (let i = 0; i < images.length; i++) {
-            images[i].id = baseId + "-" + i;
+          console.log(images[i])
+          images[i].id = baseId + "-" + i;
         }
         this.loadingCount = images.length,
         this.images = images
@@ -226,6 +172,14 @@ page {
   display: flex;
   flex-direction: column;
   background: #faecc7;
+  .title {
+    width: 100%;
+    color: #fff;
+    font-size: 22px;
+    font-weight: bold;
+    text-align: center;
+    z-index: 10;
+  }
   .top{
     width:100%;
     height:200rpx;
